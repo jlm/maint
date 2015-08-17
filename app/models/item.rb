8cf9709b70b95c96be64e7e4b2118d4dff4afbd8
John Messenger <jmessenger@iee.org>
2015-08-17 23:08:46 +0100

    Make closed items say "Closed" in the item index.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
 	has_many :minutes, as: :minuteable
 	before_save { self.latest_status = self.minutes.where("DATE is not null").order(:date).last.status }
+
+	def is_closed?
+		["P", "J", "W"].include? self.latest_status
+	end
 end
