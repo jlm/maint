da80073f64885f31e81a3271f686ada77b78be4a
John Messenger <jmessenger@iee.org>
2015-09-08 08:23:46 +0100

    CanCan causes an error when executing the create method.  Workaround applied. Fixes #13.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+	# Workaround for CanCan from https://github.com/ryanb/cancan/issues/835#issuecomment-18663815
+	before_filter do
+		resource = controller_name.singularize.to_sym
+		method = "#{resource}_params"
+		params[resource] &&= send(method) if respond_to?(method, true)
+	end
+
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
