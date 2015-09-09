c0038e57c3fda87d4d5fe629f80cb8fb46586330
John Messenger <jmessenger@iee.org>
2015-09-09 06:36:21 +0100

    Issue #16: fix latest_status Order minutes entries by meeting date when calculating the latest_status of an item. Fixes #16.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ActiveRecord::Base
 	has_many :minutes, as: :minuteable
-	before_save { lastminute = self.minutes.where("DATE is not null").order(:date).last; self.latest_status = lastminute.status unless lastminute.nil? }
+	before_save {
+		lastminute = self.minutes.where("minutes.DATE is not null").joins(:meetings).order("meetings.DATE").last;
+		self.latest_status = lastminute.status unless lastminute.nil?
+	}
 	scope :closed, -> { where latest_status: ["P", "J", "W"] }
 	scope :open, ->   { where.not latest_status: ["P", "J", "W"]  }
 
