c1c213f0aee53fd21253881765a0b0b0c29cc506
John Messenger <jmessenger@iee.org>
2015-09-11 00:06:15 +0100

    Add validations for the models, in preparation for better data entry.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ActiveRecord::Base
 	has_many :minutes, as: :minuteable
 	has_and_belongs_to_many :minutes
+	validates :date, presence: true, uniqueness: { message: "A meeting on that date already exists" }
+	validates :meetingtype, format: { with: /\A([iI]nterim|[pP]lenary)\z/, message: "must be Interim or Plenary" }
 end
--- app/models/minute.rb
+++ app/models/minute.rb
@@ -1,4 +1,5 @@
 class Minute < ActiveRecord::Base
 	belongs_to :minuteable, polymorphic: true
 	has_and_belongs_to_many :meetings
+	validates :date, presence: true
 end
