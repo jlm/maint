d663ea4bb3b02e828f49357b98b34a36610f9663
John Messenger <jmessenger@iee.org>
2015-09-28 18:14:19 +0100

    Prepare for using with Kuberantes.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,3 @@
-FROM ruby:2.2.3
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev
-RUN mkdir /myapp
-WORKDIR /myapp
-ADD Gemfile /myapp/Gemfile
-RUN bundle install
-ADD . /myapp
+FROM rails:onbuild
+ENV RAILS_ENV=production
+CMD ["sh", "/usr/src/app/init.sh"]
---
+++ init.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY_BASE=$(bundle exec rake secret)
+export DEVISE_PEPPER=$(bundle exec rake secret)
+export RAILS_SERVE_STATIC_FILES='true'
+. secrets.sh
+
+bundle exec rake db:create db:migrate
+bundle exec rake assets:precompile
+bundle exec rails s Puma -p 3000 -b '0.0.0.0'
