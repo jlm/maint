5cd9c623d66a261a24d03365450c9de95ed929d2
John Messenger <jmessenger@iee.org>
2016-01-26 11:21:50 -0700

    Get the secret key base for devise from an environment variable.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
+  config.secret_key = ENV['SECRET_KEY_BASE']
   # config.secret_key = '76fcb120cf5f86868873c0b068f5762eeee98ed8816e973c6b58cad56cce6bf43ba83e7c4020c4602d16b977127f89fbb2ded36a9e0cce5c5737c0c16e8ad675'
 
   # ==> Mailer Configuration
