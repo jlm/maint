52cc48d7128079c41b419b65522ff94f61b5fbf0
John Messenger <jmessenger@iee.org>
2016-01-28 10:50:50 -0700

    Support an alternate docker configuration. Changes to support two different docker environments with different HOST_PORT settings on the same host. Note that you still have to change Dockerfile (for the RAILS_ENV) and docker-compose.yml (for the container name) manually to build the alternate configuration.

---
+++ config/environments/docker3.rb
@@ -0,0 +1,8 @@
+# The Docker docker3 environment is a clone of the docker one, with a few changes.
+require_relative 'docker'
+Rails.application.configure do
+  #config.cache_store = :dalli_store, ENV['MEMCACHED_SERVICE_HOST'], { namespace: 'maint' }
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+end
