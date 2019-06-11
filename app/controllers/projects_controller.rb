8d1e585c599aaa69728b74a21c218cb9f4994170
John Messenger <jmessenger@iee.org>
2019-06-11 18:29:01 +0100

    Bugfix show_timeline_by_desig

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   load_and_authorize_resource
-  skip_authorize_resource only: :show_timeline
+  skip_authorize_resource only: [:show_timeline, :show_timeline_by_desig]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   respond_to :html, :json
 
