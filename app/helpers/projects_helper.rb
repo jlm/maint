6e0666c39a41ba05669520e20d9905e9f1141f2a
jlm <j.l.messenger@ieee.org>
2021-12-11 00:40:15 +0000

    Rubocop compliance

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Helper methods for the Projects controller.
 module ProjectsHelper
   def par_expiry_date(project)
     project&.events&.where(name: 'PAR Expiry')&.first&.date || ''
