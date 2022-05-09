3bfe7de0ef06101e66edac019f00b7ce7a4ee525
jlm <j.l.messenger@ieee.org>
2022-05-09 21:41:45 +0100

    Print a message saying what the ExecJS runtime is.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ENV['HOST_PORT'] ||= ":80"
+mylog = Logger.new(STDERR)
+mylog.info("environment.rb: The ExecJS runtime is #{ExecJS.runtime.name}")
