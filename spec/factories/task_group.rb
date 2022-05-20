aeb118fcfaf5b187cabc96f346d846ef16489971
jlm <j.l.messenger@ieee.org>
2022-05-20 22:34:30 +0100

    Include API tests for /items

--- spec/factories/task_group.rb
+++ spec/factories/task_group.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :task_group do
     chair
-    abbrev { 'PRN' }
-    name { 'Pruning' }
-    page_url { 'https://www.ieee802.org/43/task_groups/pruning/' }
+    abbrev { generate_text(3) }
+    name { Faker::Movie.title }
+    page_url { "https://www.ieee802.org/#{Faker::Number.between(from: 0, to: 14)}/groups/#{Faker::Number.between(from: 1, to: 40)}/" }
   end
 end
---
+++ spec/support/alphabetic.rb
@@ -0,0 +1,5 @@
+# Generate an alphabetic string of a given length
+def generate_text(number)
+  charset = Array('A'..'Z') + Array('a'..'z')
+  Array.new(number) { charset.sample }.join
+end
